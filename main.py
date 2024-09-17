6f92c3cd3f7e4d913308579f6ba4a4ca55d82353
PabloLandro <landrovepablo@gmail.com>
2024-09-17 15:31:30 +0200

    pyserini

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from gpt import chatgpt_query
+from pyserini.search import LuceneSearcher
+
+
+print('hello')
+searcher = LuceneSearcher('/mnt/beegfs/groups/irgroup/indexes/CC-NEWS-TREC-misinfo-2020')
+doc = searcher.doc('en.noclean.c4-train.00005-of-07168.143426')
+print(doc.raw())
 
 print("Write your query")
 
 prompt = input("> ")
+
 
 print(chatgpt_query(prompt))
