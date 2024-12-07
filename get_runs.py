ad9178f6fd8eed973aef6de40bb657f90361d276
PabloLandro <landrovepablo@gmail.com>
2024-12-08 00:20:46 +0100

    Added 40-mini runs

--- get_runs.py
+++ get_runs.py
@@ -56,9 +56,9 @@ def get_run_list(topic_list):
             run_list[topic_id].append(doc_id)
     return run_list
 
-def run_run_list(run_list, num_runs=300):
+def run_run_list(run_list, num_runs=300, directory="runs"):
     for topic_id, doc_ids in run_list.items():
-        with open(os.path.join("runs", topic_id), "a") as file:
+        with open(os.path.join(directory, topic_id), "a") as file:
             for doc_id in doc_ids:
                 if qrels[topic_id][doc_id]['u'] == 0 and count0[topic_id] >= num_runs:
                     continue
@@ -75,12 +75,47 @@ def run_run_list(run_list, num_runs=300):
                     print(f"Writing to {os.path.join('runs', topic_id)}")
                     file.write(f'{doc_id} {run["u"]} {run["s"]} {run["cr"]}\n')
 
+# Gives a run_list to run the same (topic,doc) pairs as another runs folder
+def copy_run_list(directory):
+    run_list = {}
+    for topic_id in os.listdir(directory):
+        run_list[topic_id] = []
+        with open(os.path.join(directory, topic_id), "r") as file:
+            for line in file:
+                doc_id,_,_,_ = line.split()
+                run_list[topic_id].append(doc_id)
+    return run_list
+
+# Replaces all elements in directory that are present in the run_list with new runs
+def replace_with_run_list(run_list, directory):
+    for topic_id in os.listdir(directory):
+        if topic_id not in run_list:
+            continue
+        with open(os.path.join(directory, topic_id), "r") as file, open("auxfile", "w") as auxfile:
+            for line in file:
+                doc_id,_,_,_ = line.split()
+                if doc_id not in run_list[topic_id]:
+                    auxfile.write(line)
+                else:
+                    doc = json.loads(searcher.doc(doc_id).raw())["text"]
+                    print(f"Evaluating {topic_id} {doc_id}")
+                    run = evaluate(topics[topic_id]["description"], doc)
+                    if run is not None:
+                        auxfile.write(f'{doc_id} {run["u"]} {run["s"]} {run["cr"]}\n')
+                        print(f"Writing to {os.path.join('runs', topic_id)}")
+        os.replace("auxfile", os.path.join(directory, topic_id))
+
+
+
 # topic_id, doc_id pairs we will run
 run_list = {}
-topic_list = os.listdir("runs")
-run_list = get_run_list(topic_list=topic_list)
+#topic_list = os.listdir("runs")
+#run_list = get_run_list(topic_list=topic_list)
+run_list = copy_run_list("aux")
 
 print("RUN LIST OBTAINED")
-#print(run_list)
+print(run_list)
 
-run_run_list(run_list)
+#run_run_list(run_list)
+replace_with_run_list(run_list, "runs")
+
