a6baf0cf6ef791b65c6590a4a4ef17c61553dfa2
PabloLandro <landrovepablo@gmail.com>
2025-02-03 17:47:30 +0100

    Clean runs

--- clean_runs.py
+++ clean_runs.py
@@ -1,16 +1,31 @@
 import os
 from trec_utils import get_qrels_dict_all
 
-# This script removes any runs from FOLDER that are not present in any qrel
+# This script removes any duplicate runs or runs from FOLDER that are not present in any qrel
 
-FOLDER = "runs"
+FOLDER = "runs/runs_features_v1"
 
 qrels = get_qrels_dict_all()
 
-for topic_id in os.listdir(FOLDER):
-    with open(os.path.join(FOLDER, topic_id), "r") as infile, open("aux", "w") as outfile:
-        for line in infile:
-            doc_id,_,_,_ = line.split()
-            if doc_id in qrels[topic_id]:
-                outfile.write(line)
-    os.replace("aux", os.path.join(FOLDER, topic_id))
+#for topic_id in os.listdir(FOLDER):
+#    visited = []
+#    with open(os.path.join(FOLDER, topic_id), "r") as infile, open("aux", "w") as outfile:
+#        for line in infile:
+#            doc_id,_,_,_ = line.split()
+#            if doc_id in qrels[topic_id] and doc_id not in visited:
+#                outfile.write(line)
+#            visited.append(doc_id)
+#    os.replace("aux", os.path.join(FOLDER, topic_id))
+
+
+
+for name in os.listdir(FOLDER):
+    for topic_id in os.listdir(os.path.join(FOLDER,name)):
+        visited = []
+        with open(os.path.join(FOLDER, name, topic_id), "r") as infile, open("aux", "w") as outfile:
+            for line in infile:
+                doc_id,_,_,_ = line.split()
+                if doc_id in qrels[topic_id] and doc_id not in visited:
+                    outfile.write(line)
+                visited.append(doc_id)
+        os.replace("aux", os.path.join(FOLDER, name, topic_id)) 
