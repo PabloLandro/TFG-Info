279bff648b9f7de8d6917a84be1484bb1b2701f2
PabloLandro <landrovepablo@gmail.com>
2025-05-08 10:27:14 +0200

    remove pycache

--- test_ollama.py
+++ test_ollama.py
@@ -1,12 +1,25 @@
-import ollama, os
+import ollama, os,sys
 from trec_utils import set_year, get_year_data, get_doc_content, read_gpt_output, write_run_to_file
 from gpt import fill_prompt
+from llama3_tokenizer import get_tokenizer
 
 set_year(2021)
 qrels,topics = get_year_data()
 
 doc_id = None
 count = 1
+
+prompt_template = ""
+with open(os.path.join("resources", "prompts", "DesDelStr_2021.txt"), "r") as file:
+    prompt_template = file.read()
+
+tokenizer = get_tokenizer("llama3-8b-instruct-q4_1")
+
+tokens = tokenizer.encode(prompt_template, return_tensors="pt")  # Use 'return_tensors' for PyTorch tensors
+
+# Tokens contain input_ids, attention_mask, etc.
+print(tokens)
+sys.exit(0)
 with open(os.path.join("runs", "DesDelStr_2021_llama3"), "a") as out_file:
     for id in qrels["101"].keys():
         if count <= 17:
@@ -14,9 +27,7 @@ with open(os.path.join("runs", "DesDelStr_2021_llama3"), "a") as out_file:
             continue
         doc_id = id
         doc = get_doc_content(doc_id)
-        prompt_template = ""
-        with open(os.path.join("resources", "prompts", "DesDelStr_2021.txt"), "r") as file:
-            prompt_template = file.read()
+        
 
         prompt = fill_prompt(topics["101"]["query"], topics["101"]["description"], topics["101"]["narrative"], doc, prompt_template)
 
