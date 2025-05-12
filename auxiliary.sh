b5b4d2d66c734f588cfeab2ec16097f4bebf81ab
PabloLandro <landrovepablo@gmail.com>
2025-05-12 15:33:39 +0200

    updated auxiliary

--- auxiliary.sh
+++ auxiliary.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=tfm           # Job name
-#SBATCH --nodelist=hpc-gpu4
+#SBATCH --nodelist=hpc-gpu1
 #SBATCH --nodes=1                    # -N Run all processes on a single node
 #SBATCH --ntasks=1                   # -n Run a single task
 #SBATCH --cpus-per-task=32            # -c Run 1 processor per task
 #SBATCH --gres=gpu:A100_80:1
 ##SBATCH --constraint=cpu_amd
 #SBATCH --mem=40G                    # Job memory request
-#SBATCH --time=00:40:00              # Time limit hrs:min:sec
+#SBATCH --time=00:30:00              # Time limit hrs:min:sec
 #SBATCH --qos=regular                 # Cola
 #SBATCH --output=log_%x_%j.log       # Standard output and error log
 
 
-# source ~/.bashrc
-# conda init bash
-# conda activate myTrecEnv
-# export LC_ALL=C.UTF-8
-# export LANG=C.UTF-8
+source ~/.bashrc
+conda init bash
+conda activate myTrecEnv
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+nohup start_ollama.sh &
 
 #ptyhon get_runs --year <year> <prompt_file> <output_folder/file> <topics (101,102,... or all)> --model <gpt|llama3>
 python get_runs.py --year 2021 resources/prompts/DesDelStr_2021.txt runs/DesDelStr_2021_llama3 all --model llama3
